5218a10261623d20d1c4defa5c2ac75254c6e809
Carlos H-E <carloshuerta.enciso@gmail.com>
2026-05-09 17:23:01 -0500

    Update chapter2.py

--- chapter2.py
+++ chapter2.py
@@ -1,5 +1,14 @@
 #Here are the notes from chapter 2 from the book. 
 
-
+#This is python code, where you can "print" messages using "" marks and using the print fucntion 
+#Here is an example:
 print("Hello World")
+
+
+#Variables are containers that house a value in which they reference. You can assign a value for a variable and call it 
+#When you need it. Here is an example: 
+
+message = "Hello Python World!" # "Hello Python World" is the value and "message" is the variable. 
+
+print (message) 
 
