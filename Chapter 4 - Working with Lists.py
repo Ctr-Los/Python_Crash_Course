9b3e6dcba271ab133cdaa49957580b0939bef2ed
Carlos H-E <carloshuerta.enciso@gmail.com>
2026-05-18 16:04:41 -0500

    Update Chapter 4 - Working with Lists.py

--- Chapter 4 - Working with Lists.py
+++ Chapter 4 - Working with Lists.py
@@ -1,1 +1,13 @@
+# Loops 
+
+#Looping lets you work with all the items on the list without the need to execute each item one at a time. 
+#This can be applied to a list with even millions of items!
+#Do perform the same action with every item in the list, you can use a "for loop"
+
+#Here is an example code of a list of magicians names and we want to print their names all at the same time. 
+
+
+magicians = ['alice', 'david','carolina']
+for magician in magicians: #Here we defined a for loop, this tells python to pull all the name from the list magicians and then store them into the variable "magician"
+  print(magician) #It reads the code as " For every magician in the list of magicians, print the magicians name"
 
