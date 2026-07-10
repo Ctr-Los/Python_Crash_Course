a0c132543c051c46c9ecd11fdb56c723cdc2664d
Carlos H-E <carloshuerta.enciso@gmail.com>
2026-07-09 22:26:02 -0500

    Update Chapter 6 Dictionaries.py

--- Chapter 6 Dictionaries.py
+++ Chapter 6 Dictionaries.py
@@ -9,3 +9,8 @@
 alien_0 = {'color': 'green', 'points': 5}
 print(alien_0['color'])
 print(alien_0['points'])
+
+#A dictionary is a collection of "Key-value" pairs. Basically each "key" is connected to a "value".
+#A dictionary is wrapped with a {} braces. Inside you put the key-value pairs.
+#'color' is the KEY and 'green' is the VALUE
+#Each key-value pair is seperated by a comma and you can add as many of them as you like!
